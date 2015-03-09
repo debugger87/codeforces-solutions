6738c878510ca12ae1a3b4ef8b0957ec6adbd6f8
debugger87 <yangchaozhong.2009@gmail.com>
2015-03-09 18:24:55 +0800

    update CF3B

--- cpp/CF3B.cpp
+++ cpp/CF3B.cpp
@@ -32,7 +32,7 @@ int main(){
       node[i].v = y;
       x == 1 ? node[i].w = 1 : node[i].w = 2;
     }
-    sort(node+1, node+n+1, cmp);
+    sort(node + 1, node + n + 1, cmp);
     int MAX = 0, w1 = 0, v1 = 0, MAXi = 1;
     for(int i = 1, j = 1; i <= n; i++){
       w1 += node[i].w;
@@ -44,7 +44,7 @@ int main(){
       }
       if(v1 > MAX){
         MAX = v1;
-        MAXi=j;
+        MAXi = j;
       }
     }
     printf("%d\n", MAX);
